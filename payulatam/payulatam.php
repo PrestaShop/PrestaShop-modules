42ebe5fab13e054e796c68dff742c442e4f0a3c1
François Gaillard <francois.gaillard@prestashop.com>
2012-12-10 14:40:07 +0100

    // PayU Latam, version number changed

--- payulatam/payulatam.php
+++ payulatam/payulatam.php
@@ -39,7 +39,7 @@ class PayULatam extends PaymentModule
 	{
 		$this->name = 'payulatam';
 		$this->tab = 'payments_gateways';
-		$this->version = '1.1';
+		$this->version = '1.2';
 		$this->author = 'PrestaShop';
 
 		parent::__construct();
