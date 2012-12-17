384c55e5add10e45d2c4e7be5f982fdda94cc13f
Damien Metzger <damien.metzger@prestashop.com>
2012-12-17 16:58:24 +0100

    // And version number

--- authorizeaim/authorizeaim.php
+++ authorizeaim/authorizeaim.php
@@ -33,7 +33,7 @@ class authorizeAIM extends PaymentModule
 	{
 		$this->name = 'authorizeaim';
 		$this->tab = 'payments_gateways';
-		$this->version = '1.4.3';
+		$this->version = '1.4.4';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
