6323f5ae485a6d3e4410b2a0b0fb92e507925552
Francois Gaillard <francois.gaillard@prestashop.com>
2013-01-08 17:39:18 +0100

    [-] MO : Bug fix #PNM-567 : PayPal, customer table creation on upgrade

--- paypal/upgrade/install-3.0.php
+++ paypal/upgrade/install-3.0.php
@@ -67,6 +67,17 @@ function upgrade_module_3_0($object, $install = false)
 			if (!Db::getInstance()->ExecuteS('SHOW COLUMNS FROM `'._DB_PREFIX_.'paypal_order` LIKE \''.pSQL($column['name']).'\''))
 				Db::getInstance()->Execute('ALTER TABLE `'._DB_PREFIX_.'paypal_order` ADD `'.pSQL($column['name']).'` '.$column['type']);
 	}
+	
+	if (count(Db::getInstance()->ExecuteS('SHOW TABLES FROM `'._DB_NAME_.'` LIKE \''._DB_PREFIX_.'paypal_customer\'')) <= 0)
+	{
+		Db::getInstance()->Execute('
+			CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'paypal_customer` (
+				`id_paypal_customer` int(10) unsigned NOT NULL AUTO_INCREMENT,
+				`id_customer` int(10) unsigned NOT NULL,
+				`paypal_email` varchar(255) NOT NULL,
+				PRIMARY KEY (`id_paypal_customer`)
+			) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8 AUTO_INCREMENT=1');
+	}
 
 	return true;
 }
