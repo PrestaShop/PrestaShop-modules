ea79544f3298f1a16a57c4eee60f4144cace2c92
gRoussac <gregory.roussac@prestashop.com>
2013-01-17 15:47:10 +0100

    [*] MO : New version of mobile_theme module (0.5.0)

--- mobile_theme/prestashop_mobile/modules/blockcategories/fr.php
+++ mobile_theme/prestashop_mobile/modules/blockcategories/fr.php
@@ -2,4 +2,4 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{blockcategories}prestashop_mobile>blockcategories_af1b98adf7f686b84cd0b443e022b7a0'] = 'CatÃ©gories';
+$_MODULE['<{blockcategories}prestashop_mobile>blockcategories_af1b98adf7f686b84cd0b443e022b7a0'] = 'Catégories';
