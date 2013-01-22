71d60fa711f2ccc13c95eacdce974f4fee25c03d
Francois Gaillard <francois.gaillard@prestashop.com>
2013-01-22 14:35:25 +0100

    // PayPal version changed (v3.4.4)

--- paypal/paypal.php
+++ paypal/paypal.php
@@ -73,7 +73,7 @@ class PayPal extends PaymentModule
 	{
 		$this->name = 'paypal';
 		$this->tab = 'payments_gateways';
-		$this->version = '3.4.3';
+		$this->version = '3.4.4';
 
 		$this->currencies = true;
 		$this->currencies_mode = 'radio';
