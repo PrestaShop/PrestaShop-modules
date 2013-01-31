4fc213d9e374344415fb3db5d6acda5ac30b6668
gRoussac <gregory.roussac@prestashop.com>
2013-01-31 19:13:10 +0100

    [-] MO : New version of autoupgrade

--- autoupgrade/autoupgrade.php
+++ autoupgrade/autoupgrade.php
@@ -30,7 +30,7 @@ class Autoupgrade extends Module
 	{
 		$this->name = 'autoupgrade';
 		$this->tab = 'administration';
-		$this->version = '1.0.1';
+		$this->version = '1.0.2';
 		if (version_compare(_PS_VERSION_, '1.5.0.0 ', '>='))
 			$this->multishop_context = Shop::CONTEXT_ALL;
 
