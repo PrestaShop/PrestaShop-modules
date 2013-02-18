c89ff76e934d61a5254342f9c56fb49fe4ff415f
Mathieu <mathieu.bertholino@prestashop.com>
2013-02-18 10:08:50 -0500

    [*] MO : tntcarrier remove uninstall table carrier-option

--- tntcarrier/sql-uninstall.php
+++ tntcarrier/sql-uninstall.php
@@ -2,7 +2,7 @@
 
 	// Init
 	$sql = array();
-	$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'tnt_carrier_option`;';
+//	$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'tnt_carrier_option`;';
 	$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'tnt_carrier_weight`;';
 //	$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'tnt_carrier_drop_off`;';
 //$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'tnt_carrier_shipping_number`;';
