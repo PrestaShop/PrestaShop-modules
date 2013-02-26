ba02685d74828625cd3cee0e5d56a982b8274d9a
gRoussac <gregory.roussac@prestashop.com>
2013-02-26 19:42:03 +0100

    [-] MO : Can dl zip in default timeout

--- autoupgrade/classes/Tools14.php
+++ autoupgrade/classes/Tools14.php
@@ -1260,7 +1260,7 @@ class Tools14
 			$curl = curl_init();
 			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($curl, CURLOPT_URL, $url);
-			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $curl_timeout);
+			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
 			curl_setopt($curl, CURLOPT_TIMEOUT, $curl_timeout);
 			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
 			$opts = stream_context_get_options($stream_context);
