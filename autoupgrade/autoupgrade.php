622c81d54b6d439aee3997662c8f5d869a7831e5
gRoussac <gregory.roussac@prestashop.com>
2013-03-01 14:10:09 +0100

    [-] MO : Define _MYSQL_ENGINE_ for upgrade from 1.3

--- autoupgrade/autoupgrade.php
+++ autoupgrade/autoupgrade.php
@@ -30,7 +30,7 @@ class Autoupgrade extends Module
 	{
 		$this->name = 'autoupgrade';
 		$this->tab = 'administration';
-		$this->version = '1.0.8';
+		$this->version = '1.0.9';
 		
 		if (version_compare(_PS_VERSION_, '1.5.0.0 ', '>='))
 			$this->multishop_context = Shop::CONTEXT_ALL;
