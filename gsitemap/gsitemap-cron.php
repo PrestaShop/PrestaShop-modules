c49edf61cba49a903a83379ac8df35df1f696d00
Rémi Gaillard <remi.gaillard@prestashop.com>
2013-04-15 09:41:34 +0200

    // fix php notice and cron

--- gsitemap/gsitemap-cron.php
+++ gsitemap/gsitemap-cron.php
@@ -2,6 +2,9 @@
 
 include(dirname(__FILE__).'/../../config/config.inc.php');
 include(dirname(__FILE__).'/../../init.php');
+if (substr(Tools::encrypt('gsitemap/cron'), 0, 10) != Tools::getValue('token') || !Module::isInstalled('gsitemap'))
+	die('Bad token');
+
 include(dirname(__FILE__).'/gsitemap.php');
 
 $gsitemap = new Gsitemap();
