f754a095ff1cb7247268458a47fcf7bb442301b9
RaphArbuz <raphael.arbuz@watzatsong.com>
2013-08-08 12:32:43 +0200

    fixes http://forge.prestashop.com/browse/PNM-1633 by GuigZ-

--- ebay/classes/EbaySynchronizer.php
+++ ebay/classes/EbaySynchronizer.php
@@ -669,13 +669,31 @@ class EbaySynchronizer
 	{
 		$carrier = new Carrier($carrier_id);
 
-		if (Configuration::get('PS_SHIPPING_METHOD') == 1) // Shipping by weight
-			$price = $carrier->getDeliveryPriceByWeight($product->weight, $zone);
-		else // Shipping by price
-			$price = $carrier->getDeliveryPriceByPrice($product->price, $zone);
+		if ($carrier->shipping_method === 0) 
+		{ // Default
 
-		if ($carrier->shipping_handling) //Add shipping handling fee
-			$price += Configuration::get('PS_SHIPPING_HANDLING');
+			if (Configuration::get('PS_SHIPPING_METHOD') == 1) // Shipping by weight
+				$price = $carrier->getDeliveryPriceByWeight($product->weight, $zone);
+			else // Shipping by price
+				$price = $carrier->getDeliveryPriceByPrice($product->price, $zone);
+
+			if ($carrier->shipping_handling) //Add shipping handling fee
+				$price += Configuration::get('PS_SHIPPING_HANDLING');
+
+		}
+		else if ($carrier->shipping_method === 1)
+		{ // Shipping by weight
+			$price = $carrier->getDeliveryPriceByWeight($product->weight, $zone);
+		}
+		else if ($carrier->shipping_method === 2)
+		{ // Shipping by price
+			$price = $carrier->getDeliveryPriceByPrice($product->price, $zone);
+		}
+		else
+		{
+			// return 0 if is an other shipping method
+			return false;
+		}
 
 		$price += $price * Tax::getCarrierTaxRate($carrier_id) / 100;
 
