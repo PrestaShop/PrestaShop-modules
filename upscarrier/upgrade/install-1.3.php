31e69b747d08af57ab05b7f1d2596f2c18db01ad
François Gaillard <gailla.fr@gmail.com>
2013-08-20 21:21:34 +0200

    // UPS - New version released (v1.3.1)

--- upscarrier/upgrade/install-1.3.php
+++ upscarrier/upgrade/install-1.3.php
@@ -1,13 +1,14 @@
 <?php
+
 /*
-* 2007-2011 PrestaShop 
+* 2007-2013 PrestaShop
 *
 * NOTICE OF LICENSE
 *
-* This source file is subject to the Open Software License (OSL 3.0)
+* This source file is subject to the Academic Free License (AFL 3.0)
 * that is bundled with this package in the file LICENSE.txt.
 * It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/osl-3.0.php
+* http://opensource.org/licenses/afl-3.0.php
 * If you did not receive a copy of the license and are unable to
 * obtain it through the world-wide-web, please send an email
 * to license@prestashop.com so we can send you a copy immediately.
@@ -19,17 +20,21 @@
 * needs please refer to http://www.prestashop.com for more information.
 *
 *  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2011 PrestaShop SA
-*  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+*  @copyright  2007-2013 PrestaShop SA
+*  @version  Release: $Revision: 14390 $
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
-				    	
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
-						
-header("Cache-Control: no-store, no-cache, must-revalidate");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-						
-header("Location: ../");
-exit;
+
+if (!defined('_PS_VERSION_'))
+	exit;
+
+function upgrade_module_1_3($object, $install = false)
+{
+	if ($object->active || $install)
+	{
+		return Db::getInstance()->Execute('ALTER TABLE `'._DB_PREFIX_.'ups_cache` ADD INDEX ( `id_cart` , `id_carrier` , `hash` ) ;');
+	}
+	$object->upgrade_detail['1.3'][] = 'UpsCarrier upgrade error !';
+	return false;
+}
