4ac549558313995f23336d26d8066b0d01de9dd3
thoma202 <tbigueres@202-ecommerce.com>
2013-10-28 13:54:42 +0100

    [*] Fixed when updating missed informations

--- ebay/upgrade/Upgrade-1.6.php
+++ ebay/upgrade/Upgrade-1.6.php
@@ -35,6 +35,8 @@ function upgrade_module_1_6($module)
 	// Default value within
 	Configuration::updateValue('EBAY_RETURNS_WITHIN', 'Days_14');
 	Configuration::updateValue('EBAY_DELIVERY_TIME', 2);
+	Configuration::updateValue('EBAY_SYNC_PRODUCTS_MODE', Configuration::get('EBAY_SYNC_MODE'));
+	Configuration::updateValue('EBAY_SYNC_MODE', 2);
 	ebay::installPicturesSettings($module);
 
 	return true;
