8e0d8b416333558e4643b20661f7686b0cada1d2
Francois Gaillard <francois.gaillard@prestashop.com>
2013-11-27 17:32:17 +0100

    TrustPilot - New module added (v1.0)

---
+++ trustpilot/views/templates/hook/index.php
@@ -0,0 +1,36 @@
+<?php
+/*
+* 2007-2013 Profileo
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to contact@profileo.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade Profileo to newer
+* versions in the future. If you wish to customize Profileo for your
+* needs please refer to http://www.profileo.com for more information.
+*
+*  @author Profileo <contact@profileo.com>
+*  @copyright  2007-2013 Profileo
+*  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+*  International Registered Trademark & Property of Profileo
+*/
+
+			    	
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+	
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+	
+header("Location: ../");
+exit;
