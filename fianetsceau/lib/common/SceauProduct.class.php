cfd9b86e999aaa24718d2da52bb939db5f257b8d
Yann CYRILLE <yann.cyrille@fia-net.com>
2013-12-09 16:46:58 +0100

    updated with php api v4.4

---
+++ fianetsceau/lib/common/SceauProduct.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Class for the <produit> elements
+ * 
+ * @author CYRILLE Yann <yann.cyrille at fia-net.com>
+ */
+class SceauProduct extends SceauXMLElement
+{
+
+	public function __construct()
+	{
+		parent::__construct('produit');
+	}
+
+}
