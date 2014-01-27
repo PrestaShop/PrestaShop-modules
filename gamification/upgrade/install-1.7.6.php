a377e8ff391b00a61cf70fb3cc0d47362c5486fd
Vincent Augagneur <vincent.augagneur@prestashop.com>
2014-01-27 18:15:24 +0100

    //new version of gamification module - fix upgrade bug  &

--- gamification/upgrade/install-1.7.6.php
+++ gamification/upgrade/install-1.7.6.php
@@ -27,7 +27,26 @@
 if (!defined('_PS_VERSION_'))
 	exit;
 
-function upgrade_module_1_6_6($object)
+function upgrade_module_1_7_6($object)
 {
-	return Db::getInstance()->execute('ALTER TABLE `'._DB_PREFIX_.'advice` ADD `start_day` INT NULL DEFAULT 0 , ADD `stop_day` INT NULL DEFAULT 0');
+	$cols = array(
+		'start_day' => array('exist' => false, 'sql' => 'ALTER TABLE `'._DB_PREFIX_.'advice` ADD `start_day` INT NULL DEFAULT 0 '),
+		'stop_day' => array('exist' => false, 'sql' => 'ALTER TABLE `'._DB_PREFIX_.'advice` ADD `stop_day` INT NULL DEFAULT 0 '),
+		'start_date' =>  array('exist' => false, 'sql' => 'ALTER TABLE `'._DB_PREFIX_.'advice` DROP `start_date`'),
+		'stop_date' =>  array('exist' => false, 'sql' => 'ALTER TABLE `'._DB_PREFIX_.'advice` DROP `stop_date`'),
+	);
+	
+	$columns = Db::getInstance()->executeS('SHOW COLUMNS FROM `'._DB_PREFIX_.'advice` ');
+	foreach ($columns as $c)
+		if (in_array($c['Field'], array_keys($cols)))
+			$cols[$c['Field']]['exist'] = true;
+	
+	foreach ($cols as $name => $co)
+		if (in_array($name, array('start_day', 'stop_day')))
+			if (!$co['exist'])
+				Db::getInstance()->execute($co['sql']);
+		else if ($co['exist'])
+			Db::getInstance()->execute($co['sql']);
+
+	return true;
 }
