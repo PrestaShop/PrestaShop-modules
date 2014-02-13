6c94c521bac73ac0547595c1c636b8c5317bc558
Francois Gaillard <francois.gaillard@prestashop.com>
2014-02-13 18:06:17 +0100

    Google Sitemap : Version number changed (v2.3.3)

--- gsitemap/gsitemap.php
+++ gsitemap/gsitemap.php
@@ -36,7 +36,7 @@ class Gsitemap extends Module
 	{
 		$this->name = 'gsitemap';
 		$this->tab = 'seo';
-		$this->version = '2.3.2';
+		$this->version = '2.3.3';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
