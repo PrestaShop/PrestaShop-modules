a06cf87a6782f57869024fcbf126536a7aab2ba8
gRoussac <gregory.roussac@prestashop.com>
2014-02-17 15:32:59 +0100

    // productcomments update version number

--- productcomments/productcomments.php
+++ productcomments/productcomments.php
@@ -41,7 +41,7 @@ class ProductComments extends Module
 	{
 		$this->name = 'productcomments';
 		$this->tab = 'front_office_features';
-		$this->version = '2.9.3';
+		$this->version = '2.9.4';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 		$this->ps_versions_compliancy = array(
