9a1c87ad99ead16c515fd4482101f388cb30624e
Delboy <Delboy@delboy-99e38bb9>
2014-03-04 10:57:38 +0000

    Changed default button text.

--- realexredirect/translations/en.php
+++ realexredirect/translations/en.php
@@ -2,4 +2,4 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{realexredirect}prestashop>payment_66e4333017e5becf2b6c74a09ad38ea4'] = 'Pay by Credit Card with Realex Payments';
+$_MODULE['<{realexredirect}prestashop>payment_66e4333017e5becf2b6c74a09ad38ea4'] = 'Pay by Credit Card or Debit Card';
