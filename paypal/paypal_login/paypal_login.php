284387fb410465e81bac7f6237bfd322c17a76b2
Thoma Biguères <t.bigueres@gmail.com>
2014-03-10 17:46:00 +0100

    [*] Refactoring of Paypal Login evolution

--- paypal/paypal_login/paypal_login.php
+++ paypal/paypal_login/paypal_login.php
@@ -29,76 +29,34 @@ class PayPalLogin
 	private	$_logs = array();
 	private $enable_log = false;
 	
-	private $paypal = null;
+	private $paypal_connect = null;
 	
 	public function __construct()
 	{
-		$this->paypal = new PayPal();
+		$this->paypal_connect = new PayPalConnect();
 	}
 
-	public function makeConnection($host, $body = false, $http_header = false)
+	public function getIdentityAPIURL()
 	{
-		$this->_logs[] = $this->paypal->l('Making new connection to').' \''.$host.'\'';
-
-		$return = false;
-
-		if (function_exists('curl_exec'))
-			$return = $this->_connectByCURL($host, $body, $http_header);
-
-		return $return;
-	}
-
-	public function getLogs()
-	{
-		return $this->_logs;
-	}
-
-	/************************************************************/
-	/********************** CONNECT METHODS *********************/
-	/************************************************************/
-	private function _connectByCURL($url, $body = false, $http_header = false)
-	{
-		$ch = @curl_init();
-
-		if (!$ch)
-			$this->_logs[] = $this->paypal->l('Connect failed with CURL method');
+		if ( Configuration::get('PAYPAL_SANDBOX') )
+			//return 'www.sandbox.paypal.com';
+			return 'api.sandbox.paypal.com';
 		else
-		{
-			$this->_logs[] = $this->paypal->l('Connect with CURL method successful');
-			$this->_logs[] = '<b>'.$this->paypal->l('Sending this params:').'</b>';
-			$this->_logs[] = 'Post : ' . $body;
-			$this->_logs[] = 'Header : ' . print_r($http_header, true);
+			return 'api.paypal.com';
+	}
 
-			@curl_setopt($ch, CURLOPT_URL, 'https://'.$url);
-			@curl_setopt($ch, CURLOPT_USERPWD, Configuration::get('PAYPAL_LOGIN_CLIENT_ID').':'.Configuration::get('PAYPAL_LOGIN_SECRET'));
-			@curl_setopt($ch, CURLOPT_POST, true);
-			if ($body)
-			{
-				@curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-			}	
-			@curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			@curl_setopt($ch, CURLOPT_HEADER, false);
-			@curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-			@curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			@curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-			@curl_setopt($ch, CURLOPT_SSLVERSION, 3);
-			@curl_setopt($ch, CURLOPT_VERBOSE, true);
+	public function getTokenServiceEndpoint()
+	{
+		if ( Configuration::get('PAYPAL_SANDBOX') )
+			// return '/webapps/auth/protocol/openidconnect/v1/tokenservice';
+			return '/v1/identity/openidconnect/tokenservice';
+		else
+			return '/v1/identity/openidconnect/tokenservice';
+	}
 
-			if ($http_header && is_array($http_header))
-			{
-				@curl_setopt($ch, CURLOPT_HTTPHEADER, $http_header);
-			}
-
-			$result = @curl_exec($ch);
-
-			if (!$result)
-				$this->_logs[] = $this->paypal->l('Send with CURL method failed ! Error:').' '.curl_error($ch);
-			else
-				$this->_logs[] = $this->paypal->l('Send with CURL method successful');
-
-			@curl_close($ch);
-		}
-		return $result ? $result : false;
+	public function getUserInfoEndpoint()
+	{
+		return '/v1/identity/openidconnect/userinfo';
 	}
 
 	public static function getReturnLink()
@@ -106,6 +64,8 @@ class PayPalLogin
 		// return 'http://requestb.in/1jlaizq1';
 		return Context::getContext()->shop->getBaseUrl().'modules/paypal/paypal_login/paypal_login_token.php';
 	}
+
+
 
 	public function getAuthorizationCode()
 	{
@@ -116,12 +76,6 @@ class PayPalLogin
 			return $this->getRefreshToken();
 		}
 
-		if ( Configuration::get('PAYPAL_SANDBOX') )
-			$host = 'www.sandbox.paypal.com/webapps/auth/protocol/openidconnect/v1/tokenservice';
-		else
-			$host = 'api.paypal.com/v1/identity/openidconnect/tokenservice';
-
-
 		$params = array(
 			'grant_type'   => 'authorization_code',
 			'code'         => Tools::getValue('code'),
@@ -129,7 +83,7 @@ class PayPalLogin
 		);
 
 		$request = http_build_query($params, '', '&');
-		$result = $this->makeConnection($host, $request);
+		$result = $this->paypal_connect->makeConnection($this->getIdentityAPIURL(), $this->getTokenServiceEndpoint(), $request, false, false, true);
 
 		if ($this->enable_log === true)
 		{
@@ -178,18 +132,13 @@ class PayPalLogin
 		if (!is_object($login))
 			return false;
 
-		if ( Configuration::get('PAYPAL_SANDBOX') )
-			$host = 'www.sandbox.paypal.com/webapps/auth/protocol/openidconnect/v1/tokenservice';
-		else
-			$host = 'api.paypal.com/v1/identity/openidconnect/tokenservice';
-
 		$params = array(
 			'grant_type'    => 'refresh_token',
 			'refresh_token' => $login->refresh_token
 		);
 
 		$request = http_build_query($params, '', '&');
-		$result = $this->makeConnection($host, $request);
+		$result = $this->paypal_connect->makeConnection($this->getIdentityAPIURL(), $this->getTokenServiceEndpoint(), $request, false, false, true);
 
 		if ($this->enable_log === true)
 		{
@@ -217,10 +166,6 @@ class PayPalLogin
 	{
 
 		unset($this->_logs);
-		if ( Configuration::get('PAYPAL_SANDBOX') )
-			$host = 'api.sandbox.paypal.com/v1/identity/openidconnect/userinfo';
-		else
-			$host = 'api.paypal.com/v1/identity/openidconnect/userinfo';
 
 		$headers = array(
 			// 'Content-Type:application/json',
@@ -232,7 +177,7 @@ class PayPalLogin
 		);
 
 		$request = http_build_query($params, '', '&');
-		$result = $this->makeConnection($host, $request, $headers);
+		$result = $this->paypal_connect->makeConnection($this->getIdentityAPIURL(), $this->getUserInfoEndpoint(), $request, false, $headers, true);
 
 		if ($this->enable_log === true)
 		{
@@ -268,59 +213,6 @@ class PayPalLogin
 		return false;
 	}
 
-	public function getAuthorizeSemlessCheckout($client_id)
-	{
-
-		unset($this->_logs);
-		if ( Configuration::get('PAYPAL_SANDBOX') )
-			$host = 'sandbox.paypal.com/webapps/auth/protocol/openidconnect/v1/authorize';
-		else
-			$host = 'paypal.com/webapps/auth/protocol/openidconnect/v1/authorize';
-
-		$params = array(
-			'client_id'     => $client_id,
-			'response_type' => 'code',
-			'redirect_url'  => 'http://requestb.in/1jlaizq1',
-			'scope'         => 'https://uri.paypal.com/services/expresscheckout',
-		);
-
-		$request = http_build_query($params, '', '&');
-		$result = $this->makeConnection($host, $request, $params);
-
-		if ($this->enable_log === true)
-		{
-			$handle = fopen(dirname(__FILE__) . '/Results.txt', 'a+');
-			fwrite($handle, "Request => " . print_r($request, true) . "\r\n");
-			fwrite($handle, "Result => " . print_r($result, true) . "\r\n");
-			fwrite($handle, "Headers => " . print_r($params, true) . "\r\n");
-			fwrite($handle, "Journal => " . print_r($this->_logs, true."\r\n"));
-			fclose($handle);
-		}
-
-		$result = json_decode($result);
-
-		if ($result)
-		{
-			$customer = new Customer();
-			$customer = $customer->getByEmail($result->email);
-
-			if (!$customer)
-			{
-				$customer = $this->setCustomer($result);
-			}
-
-			$login->account_type = $result->account_type;
-			$login->user_id = $result->user_id;
-			$login->verified_account = $result->verified_account;
-			$login->zoneinfo = $result->zoneinfo;
-			$login->age_range = $result->age_range;
-
-			return $customer;
-		}
-
-		return false;
-
-	}
 
 	private function setCustomer($result)
 	{
@@ -342,7 +234,7 @@ class PayPalLogin
 		$address->lastname = $customer->lastname;
 		$address->firstname = $customer->firstname;
 		$address->address1 = $result_address->street_address;
-		$address->psotcode = $result_address->postal_code;
+		$address->postcode = $result_address->postal_code;
 		$address->city = $result_address->locality;
 		$address->phone = $result->phone_number;
 
