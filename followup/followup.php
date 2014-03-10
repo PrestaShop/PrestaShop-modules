bb31727929dc9be2ed075d42ddffa086c05861d2
Francois Gaillard <francois.gaillard@prestashop.com>
2014-03-10 14:09:42 +0100

    // Follow up - Version changed (v1.5)

--- followup/followup.php
+++ followup/followup.php
@@ -33,7 +33,7 @@ class Followup extends Module
 	{
 		$this->name = 'followup';
 		$this->tab = 'advertising_marketing';
-		$this->version = '1.4';
+		$this->version = '1.5';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
