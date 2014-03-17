d13d1110acd6f067f0744d8aaa2523400014a425
Yann CYRILLE <yann.cyrille@fia-net.com>
2014-03-17 11:19:53 +0100

    Adaptation code for Prestashop validation

--- fianetsceau/subcategory.php
+++ fianetsceau/subcategory.php
@@ -1,7 +1,6 @@
 <?php
-
-/*
- * 2007-2013 PrestaShop
+/**
+ * 2007-2014 PrestaShop
  *
  * NOTICE OF LICENSE
  *
@@ -18,10 +17,10 @@
  * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to http://www.prestashop.com for more information.
- *
- *  @author PrestaShop SA <contact@prestashop.com>
- *  @copyright  2007-2013 PrestaShop SA
- *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * 
+ *  @author    PrestaShop SA <contact@prestashop.com>
+ *  @copyright 2007-2014 PrestaShop SA
+ *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *  International Registered Trademark & Property of PrestaShop SA
  */
 
@@ -49,17 +48,17 @@ if (Tools::getValue('token') == Tools::getAdminToken($sceau->getSiteid().$sceau-
 	$subcategory = $module->loadFianetSubCategories($category_id);
 
 	if ($category_id == '0')
-	{
-		$select = "";
-	} else
+		$select = '';
+	else
 	{
 		$select = "<select id='fianetsceau_".$elem_id."_subcategory' name='fianetsceau_".$elem_id."_subcategory'>";
 		foreach ($subcategory as $value)
-			$select .= "<option value=".$value['subcategory_id'].">".$value['label']."</option>";
+			$select .= '<option value='.$value['subcategory_id'].'>'.$value['label'].'</option>';
 
-		$select .= "</select>";
+		$select .= '</select>';
 	}
 
 	echo $select;
-} else
-	header("Location: ../");
+}
+else
+	header('Location: ../');
