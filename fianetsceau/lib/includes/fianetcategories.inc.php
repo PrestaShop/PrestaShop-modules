002f350451e322d67ca71cf56ab32001de2a669f
Yann CYRILLE <yann.cyrille@fia-net.com>
2014-03-17 11:08:40 +0100

    Adaptation code for Prestashop validation

--- fianetsceau/lib/includes/fianetcategories.inc.php
+++ fianetsceau/lib/includes/fianetcategories.inc.php
@@ -1,11 +1,35 @@
 <?php
+/**
+ * 2007-2014 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author    PrestaShop SA <contact@prestashop.com>
+ *  @copyright 2007-2014 PrestaShop SA
+ *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *  International Registered Trademark & Property of PrestaShop SA
+ */
 
 /**
  * FIA-NET categories
  * 
  * @var array
  */
-$_fianetsceau_categories = array(
+$fianetsceau_categories = array(
 	1 => 'Alimentation & Gastronomie',
 	2 => 'Animaux',
 	3 => 'Armurerie',
@@ -36,7 +60,7 @@ $_fianetsceau_categories = array(
  * 
  * @var array
  */
-$_fianetsceau_subcategories = array(
+$fianetsceau_subcategories = array(
 	2 => array('parent_id' => 1, 'label' => 'Chocolats & Confiseries'),
 	3 => array('parent_id' => 1, 'label' => 'Alimentation g&eacute;n&eacute;rale'),
 	4 => array('parent_id' => 1, 'label' => 'Produits du terroir'),
@@ -317,6 +341,4 @@ $_fianetsceau_subcategories = array(
 	138 => array('parent_id' => 22, 'label' => 'Week-ends'),
 	139 => array('parent_id' => 22, 'label' => 'Cartes & Guides'),
 	140 => array('parent_id' => 22, 'label' => 'Derni&egrave;re minute'),
-	141 => array('parent_id' => 22, 'label' => 'Bagagerie'),
-);
-
+	141 => array('parent_id' => 22, 'label' => 'Bagagerie'));
