f22954e3d39ab50948e717bac71161853113e569
Quetzacoalt91 <thomas.nabord@prestashop.com>
2014-03-19 10:16:14 +0100

    // MO Authorize.net : Version update

--- authorizeaim/authorizeaim.php
+++ authorizeaim/authorizeaim.php
@@ -33,7 +33,7 @@ class authorizeAIM extends PaymentModule
 	{
 		$this->name = 'authorizeaim';
 		$this->tab = 'payments_gateways';
-		$this->version = '1.4.9';
+		$this->version = '1.4.10';
 		$this->author = 'PrestaShop';
 		$this->aim_available_currencies = array('USD','AUD','CAD','EUR','GBP','NZD');
 
