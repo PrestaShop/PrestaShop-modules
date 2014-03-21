e412ae753e3cd0f0d1c98a22d4dc3053a9fb260f
Quetzacoalt91 <thomas.nabord@prestashop.com>
2014-03-21 17:53:02 +0100

    // MO Google Sitemap : ps_versions_compliancy added

--- gsitemap/gsitemap.php
+++ gsitemap/gsitemap.php
@@ -36,14 +36,14 @@ class Gsitemap extends Module
 	{
 		$this->name = 'gsitemap';
 		$this->tab = 'seo';
-		$this->version = '2.3.6';
+		$this->version = '2.3.7';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
 		parent::__construct();
 
 		$this->ps_versions_compliancy = array(
-			'min' => '1.5.0.0',
+			'min' => '1.5.6.1',
 			'max' => _PS_VERSION_
 		);
 
