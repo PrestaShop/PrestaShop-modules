5fe3f652589e09342bbbcd0ef7fcb8e4c26dff67
Quetzacoalt91 <thomas.nabord@prestashop.com>
2014-03-28 16:06:48 +0100

    // MO nqgatewayneteven : ps_versions_compliancy deleted (Bug with PS 1.5.3.1)

--- nqgatewayneteven/nqgatewayneteven.php
+++ nqgatewayneteven/nqgatewayneteven.php
@@ -46,14 +46,14 @@ class NqGatewayNeteven extends Module
 
         $this->tab = $tab_name;
 		
-		$this->version = '2.8';
+		$this->version = '2.9';
 		$this->author = 'NetEven';
 		
 		parent::__construct();
 
 		$this->displayName = $this->l('NetEven');
 		$this->description = $this->l('Vendez sur toutes les marketplaces depuis votre PrestaShop');
-		$this->ps_versions_compliancy = array('min' => '1.4', 'max' => _PS_VERSION_);
+
 
 		$this->feature_url = '/script/set-neteven-categories.php?token='.Tools::encrypt(Configuration::get('PS_SHOP_NAME'));
 		$this->order_url = '/script/import-order.php?token='.Tools::encrypt(Configuration::get('PS_SHOP_NAME')).'&active=1';
