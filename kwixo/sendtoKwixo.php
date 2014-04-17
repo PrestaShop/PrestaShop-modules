24e1873214614612690493e1a71d4dc5a54578f3
Yann <yann.cyrille@fia-net.com>
2014-04-17 16:32:17 +0200

    Update sendtoKwixo.php

--- kwixo/sendtoKwixo.php
+++ kwixo/sendtoKwixo.php
@@ -27,7 +27,7 @@
 require_once '../../config/settings.inc.php';
 require_once '../../config/defines.inc.php';
 
-if (_PS_VERSION_ < '1.5')
+if (version_compare(_PS_VERSION_, '1.5', '<'))
 {
 	require_once 'KwixoFrontController.php';
 	$kwixo = new KwixoPayment();
