73b2e44adecf1e85f2d534f2fbe90e24998e7f5a
Thoma Biguères <t.bigueres@gmail.com>
2014-07-15 15:15:54 +0200

    [*] MO - eBay : initialize when CRON for 1.4

--- ebay/synchronizeOrders_CRON.php
+++ ebay/synchronizeOrders_CRON.php
@@ -2,6 +2,7 @@
 
 
 include(dirname(__FILE__) . '/../../config/config.inc.php');
+include(dirname(__FILE__) . '/../../init.php');)
 include(dirname(__FILE__) . '/ebay.php');
 
 class ebaySynchronizeOrdersTask extends Ebay {
