7b0746c5286e2a2b0a19ba5b6a63edd2fdcc682b
Thomas Nabord <thomas.nabord@prestashop.com>
2014-07-30 17:27:47 +0200

    [-] PayPal USA : URL for Order confirmation fixed (PR #376)

--- paypalusa/paypalusa.php
+++ paypalusa/paypalusa.php
@@ -377,7 +377,7 @@ class PayPalUSA extends PaymentModule
 				'paypal_usa_cancel_url' => $this->context->link->getPageLink('order.php',''),
 				'paypal_usa_notify_url' => $this->context->link->getModuleLink('paypalusa', 'validation', array('pps' => 1), Configuration::get('PS_SSL_ENABLED')), 
 				'paypal_usa_return_url' => /*26/12/2013 fix for Backward compatibilies on confirmation page*/
-					((int)version_compare(_PS_VERSION_, '1.4', '>')) ?
+					version_compare(_PS_VERSION_, '1.5', '<') ?
 					(Configuration::get('PS_SSL_ENABLED') ? Tools::getShopDomainSsl(true) : Tools::getShopDomain(true)).
 					__PS_BASE_URI__.'order-confirmation.php?id_cart='.(int)$this->context->cart->id.'&id_module='.(int)$this->id.'&key='.$this->context->customer->secure_key : 
 					$this->context->link->getPageLink('order-confirmation.php', null, null, array('id_cart' => (int)$this->context->cart->id, 'key' => $this->context->customer->secure_key, 'id_module' => $this->id)),
