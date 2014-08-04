a4b6ceb240ce22ee82d0ac10fed31500d78bcbc2
Quadra Informatique <modules@quadra-informatique.fr>
2014-08-04 14:30:10 +0200

    [*] MO : So Colissimo version 2.9.5

--- socolissimo/socolissimo.php
+++ socolissimo/socolissimo.php
@@ -65,7 +65,7 @@ class Socolissimo extends CarrierModule
     {
         $this->name = 'socolissimo';
         $this->tab = 'shipping_logistics';
-        $this->version = '2.9.4';
+        $this->version = '2.9.5';
         $this->author = 'Quadra Informatique';
         $this->limited_countries = array('fr');
         $this->module_key = 'faa857ecf7579947c8eee2d9b3d1fb04';
