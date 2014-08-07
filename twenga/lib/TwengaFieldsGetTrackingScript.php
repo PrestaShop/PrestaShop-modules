4f25f9b998970eabd1dd53ef0eb9e310f06383fb
Claude Traglia <claude.traglia@twenga.com>
2014-08-07 11:40:12 +0200

    // Twenga : Release of version 2.0

--- twenga/lib/TwengaFieldsGetTrackingScript.php
+++ twenga/lib/TwengaFieldsGetTrackingScript.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 2007-2014 PrestaShop
+ * 2007-2013 PrestaShop
  *
  * NOTICE OF LICENSE
  *
@@ -19,7 +19,7 @@
  * needs please refer to http://www.prestashop.com for more information.
  *
  *  @author PrestaShop SA <contact@prestashop.com>
- *  @copyright 2007-2014 PrestaShop SA : 6 rue lacepede, 75005 PARIS
+ *  @copyright 2007-2013 PrestaShop SA : 6 rue lacepede, 75005 PARIS
  *  @version  Release: $Revision: 14011 $
  *  @license	http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *  International Registered Trademark & Property of PrestaShop SA
@@ -33,32 +33,48 @@ class TwengaFieldsGetTrackingScript extends TwengaFields
 		{
 			// required
 			$this->fields['PARTNER_AUTH_KEY'] = array(56, array('is_string', 'isCleanHtml'), true);
+			$this->fields['is_customer'] = array(56, array('isInt'));
+			$this->fields['ecommerce_platform'] = array(56, array('is_string','isCleanHtml'));
+			$this->fields['tag_platform'] = array(56, array('is_string','isCleanHtml'));
+			$this->fields['event'] = array(32, array('is_string','isCleanHtml'), true);
 			$this->fields['key'] = array(32, array('is_string','isCleanHtml'), true);
 			$this->fields['total_ht'] = array(0, array('isFloat'), true);
 			$this->fields['order_id'] = array(40, array('isInt'));
 			$this->fields['user_id'] = array(40, array('isInt'));
 			$this->fields['cli_email'] = array(100, array('isEmail'));
 			$this->fields['basket_id'] = array(40, array('isInt'));
-			
+
 			// optionnals
-			$this->fields['currency'] = array(40, array('is_string','isCleanHtml'));
+			$this->fields['currency'] = array(40, array('is_string','isCleanHtml'), true);
 			$this->fields['total_ttc'] = array(0, array('isFloat'));
-			$this->fields['shipping'] = array(0, array('isFloat'));
-			$this->fields['tax'] = array(0, array('isFloat'));
-			$this->fields['tva'] = array(0, array('isFloat'));
+			$this->fields['shipping'] = array(0, array('isFloat'), true);
+			$this->fields['tax'] = array(0, array('isFloat'), true);
+			$this->fields['tva'] = array(0, array('isFloat'), true);
 			$this->fields['cli_firstname'] = array(0, array('is_string','isCleanHtml'));
 			$this->fields['cli_lastname'] = array(0, array('is_string','isCleanHtml'));
+			$this->fields['cli_global_id'] = array(0, array('is_string','isCleanHtml'));
 			$this->fields['cli_city'] = array(0, array('is_string','isCleanHtml'));
 			$this->fields['cli_state'] = array(0, array('is_string','isCleanHtml'));
-			$this->fields['cli_country'] = array(0, array('is_string','isCleanHtml'));
+			$this->fields['cli_country'] = array(0, array('is_string','isCleanHtml'), true);
 			$this->fields['cli_segment'] = array(0, array('is_string','isCleanHtml'));
 			$this->fields['payement_method'] = array(0, array('is_string','isCleanHtml'));
-			
-			
+
+
 			// Array of items
 			$this->fields['items'] = array(0, array('is_array', 'isItemsOrder'));
 		}
 		parent::__construct();
+	}
+	/**
+	 * (non-PHPdoc)
+	 * @see TwengaFields::checkParams()
+	 */
+	public function checkParams()
+	{
+		parent::checkParams();
+		if(isset($this->fields['items']) && !empty($this->fields['items'])){
+			self::isItemsOrder($this->fields['items']);
+		}
 	}
 	public static function isItemsOrder ($value)
 	{
@@ -68,12 +84,12 @@ class TwengaFieldsGetTrackingScript extends TwengaFields
 			$str_error .= ' '.$key.' ';
 			$str_error .= isset($item['name']) ? $item['name'].' ' : '';
 			$str_error .= ' :';
-			if (isset($item['total_ht']) && !Validate::isFloat($item['total_ht']))
-				throw new TwengaFieldsException($str_error.Tools::displayError('The total HT must be a float value.'));
+			if (isset($item['price']) && !Validate::isFloat($item['price']))
+				throw new TwengaFieldsException($str_error.Tools::displayError('The price must be a float value.'));
 			if (isset($item['quantity']) && !Validate::isInt($item['quantity']))
 				throw new TwengaFieldsException($str_error.Tools::displayError('The quantity must be a integer value.'));
-			if (isset($item['sku']) && !is_string($item['sku']) && strlen($item['sku']) > 40)
-				throw new TwengaFieldsException($str_error.Tools::displayError('The sku must be a string with length less than 40 chars.'));
+			if (isset($item['item_id']) && empty($item['item_id']))
+				throw new TwengaFieldsException($str_error.Tools::displayError('The item_id must be a string with length less than 40 chars.'));
 			if (isset($item['name']) && !is_string($item['name']))
 				throw new TwengaFieldsException($str_error.Tools::displayError('The name must be a string with length less than 100 chars.'));
 			if (isset($item['category_name']) && !is_string($item['category_name']))
