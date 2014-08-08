81f58f7d17cf29da8f6ae15059caeb4e2f2fa4db
Claude Traglia <claude.traglia@twenga.com>
2014-08-08 15:14:15 +0200

    The licence has been amended

--- twenga/twenga.php
+++ twenga/twenga.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
 * 2007-2014 PrestaShop
 *
 * NOTICE OF LICENSE
@@ -18,10 +18,10 @@
 * versions in the future. If you wish to customize PrestaShop for your
 * needs please refer to http://www.prestashop.com for more information.
 *
-*  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2014 PrestaShop SA
-*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registred Trademark & Property of PrestaShop SA
+* @author EnvoiMoinsCher <informationapi@boxtale.com>
+* @copyright 2007-2014 PrestaShop SA / 2011-2014 EnvoiMoinsCher
+* @license http://opensource.org/licenses/afl-3.0.php Academic Free License (AFL 3.0)
+* International Registred Trademark & Property of PrestaShop SA
 */
 
 /**
