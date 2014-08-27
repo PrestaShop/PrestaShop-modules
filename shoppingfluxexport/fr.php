80ad3d93ace71f146b15723e05a508c9957749ef
Francois Gaillard <francois.gaillard@prestashop.com>
2014-08-27 18:07:47 +0200

    shoppingfluxexport : Added as a submodule

--- shoppingfluxexport/fr.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-global $_MODULE;
-$_MODULE = array();
-$_MODULE['<{shoppingfluxexport}prestashop>shoppingfluxexport_15bbb9d0bbf25e8d2978de1168c749dc'] = 'Nom du site';
-$_MODULE['<{shoppingfluxexport}prestashop>shoppingfluxexport_bc910f8bdf70f29374f496f05be0330c'] = 'Prénom';
-$_MODULE['<{shoppingfluxexport}prestashop>shoppingfluxexport_77587239bf4c54ea493c7033e1dbf636'] = 'Nom';
-$_MODULE['<{shoppingfluxexport}prestashop>shoppingfluxexport_bcc254b55c4a1babdf1dcb82c207506b'] = 'Téléphone';
-$_MODULE['<{shoppingfluxexport}prestashop>shoppingfluxexport_7d9837238c0e5b6126d668fe9b245e96'] = 'Code Prestashop (laisser vide si vous nen possédez pas)';
